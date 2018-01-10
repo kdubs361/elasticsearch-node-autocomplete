65e2ee5f27cef650bb980316d976d1350506e951
K Dubs <kdubs>
2018-01-10 11:37:49 -0500

    Fix title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Elasticsearch Tutorial' });
+    res.render('index', { title: 'Autocomplete Demo' });
 });
 
 module.exports = router;
